4101047c66de314046d44adf2d5e9a9d133348e6
rabiul3000 <mdrabiul.asia@gmail.com>
2025-08-09 14:34:30 +0600

    error page modified

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-
+import { Link } from "react-router";
+import notfoundSvg from "../../assets/notfound.svg";
 const ErrorPage = ({ error }) => {
   return (
     <div className="flex justify-center items-center h-[90vh]">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold">404 | Not Found</h1>
+      <div className="text-center w-screen h-screen flex justify-center items-center flex-col gap-4">
+        <img src={notfoundSvg} className="w-xl" alt="error_img" />
         {error && <p className="text-sm font-bold"> {error} </p>}
+        <Link to={'/'} className="btn btn-outline btn-soft btn-secondary">Go Home</Link>
       </div>
     </div>
   );
